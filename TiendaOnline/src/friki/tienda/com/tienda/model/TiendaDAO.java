4b2f57f6fd8e57afc6f2a234f38012f311b293df
unknown <poo@PUEPUE-MV4L54N7.aula.local>
2015-05-22 09:30:31 +0200

    tienda DAO

--- TiendaOnline/src/friki/tienda/com/tienda/model/TiendaDAO.java
+++ TiendaOnline/src/friki/tienda/com/tienda/model/TiendaDAO.java
@@ -1,5 +1,14 @@
 package friki.tienda.com.tienda.model;
 
+import java.util.List;
+
+import friki.tienda.com.DAO.Articulo;
+
+
 public class TiendaDAO {
-	
+	public List<Articulo> findAll(){
+		
+		return null;
+		
+	}
 }
