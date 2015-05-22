9c9dd4386dd0e64fb9aee94481fd47f36dc38623
3dmv <poo@PUEPUE-3QGJJVA1.aula.local>
2015-05-22 13:57:30 +0200

    Cliente. login

---
+++ TiendaOnline/src/friki/tienda/com/tienda/controladores/login/LoginClienteAction.java
@@ -0,0 +1,28 @@
+package friki.tienda.com.tienda.controladores.login;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
+
+
+import org.codehaus.jettison.json.JSONObject;
+
+import friki.tienda.com.global.services.Md5Encryption;
+
+import friki.tienda.com.tienda.beans.loginClienteBean;
+
+public class LoginClienteAction extends Action {
+	public ActionForward getJSONData(ActionMapping mapping, ActionForm form,
+			   HttpServletRequest request, HttpServletResponse response) {
+		response.setContentType("application/json");
+			
+				return null;
+
+
+		}
+}
