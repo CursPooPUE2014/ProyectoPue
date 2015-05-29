2f9d086ca09cea3c2e17c44b54073ccc78d25f54
3dmv <poo@PUEPUE-3QGJJVA1.aula.local>
2015-05-29 14:29:47 +0200

    añadidas las clases (ModificaUsuariocliente, EliminaUsuariocliente,ConsultaUsuariocliente)

---
+++ TiendaOnline/src/friki/tienda/com/backoffice/accions/EliminaUsuariocliente.java
@@ -0,0 +1,42 @@
+package friki.tienda.com.backoffice.accions;
+
+//import operaciones.*;
+import org.apache.struts.action.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Serializable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import friki.tienda.com.backoffice.formbeans.UsuarioclienteBean;
+
+
+public class EliminaUsuariocliente extends Action {
+
+	 public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest req, HttpServletResponse resp) {
+		
+		resp.setContentType("application/json");
+		UsuarioclienteBean usuariocliente =(UsuarioclienteBean) form;
+	
+		//conectar amb el DAO i fer un delete del usuari
+		
+		// Get the printwriter object from response to write the required json object to the output stream
+		//cridar a la classe dao per a eliminar per id 
+		PrintWriter out;
+		try {
+			out = resp.getWriter();
+		
+			out.print("Client eliminat de manera satisfactoria");
+			out.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;	//(això es necessari perquè si no retornem null, passaria el control de nou al servlet d'struts);
+
+	}
+		
+}
