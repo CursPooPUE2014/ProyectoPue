5fbc5c9780bffb2c8b4183c2c81c309d37747f08
3dmv <poo@PUEPUE-3QGJJVA1.aula.local>
2015-06-08 14:31:57 +0200

    Cambiado menu de panel de admin

--- TiendaOnline/WebContent/funcionalidad/backoffice.js
+++ TiendaOnline/WebContent/funcionalidad/backoffice.js
@@ -1,5 +1,18 @@
 function redireccionar() {
     alert('Falta Validar');
+
+
+    $.ajax( {
+        type: "POST",
+        url: "helloworld.action",  //will this declaration of action work?
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: "",
+        success: function() {
+            alert("success");
+        }
+    });
+
     window.location = "indice.html";
 }
 
