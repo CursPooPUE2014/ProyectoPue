7dc0a39b3bee764b2db208945e668db035bfad5e
3dmv <poo@PUEPUE-3QGJJVA1.aula.local>
2015-06-08 13:56:52 +0200

    Añadidas cosas nazis de la navegación

--- TiendaOnline/WebContent/funcionalidad/backoffice.js
+++ TiendaOnline/WebContent/funcionalidad/backoffice.js
@@ -1,17 +1,17 @@
-/**
- * Created by poo on 22/05/2015.
- */
-
-function logeame() {
-
-    alert($("#mail") + $("#pass"));
-
-    if(IsEmail($("#mail"))){
-
-    }
+function redireccionar() {
+    alert('Falta Validar');
+    window.location = "indice.html";
 }
 
-function IsEmail(email) {
-    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    return regex.test(email);
-}
+
+
+
+
+
+
+
+
+
+
+
+
